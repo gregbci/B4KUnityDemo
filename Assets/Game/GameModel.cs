50825fd498e130f5199cc0223f0013d26747d205
Greg Wilding <gregory.wilding@ucalgary.ca>
2024-05-01 09:36:26 -0600

    Initial commit

---
+++ Assets/Game/GameModel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModel
+{
+    // connect to data container
+    public string Name { get; set; }
+    public Color CubeColor { get; set; }
+    public Vector3 CubeRotation { get; set; } 
+
+    
+}
